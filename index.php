64b2667b6e4027f794762f86fb02d841044c8c13
Priyanka Saha <priyanka.nov15@gmail.com>
2018-02-17 04:05:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
       $ch = curl_init($url);
       curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE);
       $data = curl_exec($ch);
+      $dist = 'None';
 
       // close cURL resource, and free up system resources
       curl_close ($ch);
@@ -38,5 +39,84 @@
       print "<br>";
       print "Longitude: ";
       print_r($longitude);
-      
+
+      //make connection
+      mysql_connect('localhost', 'root', '');
+      //select db
+      mysql_select_db('slimapp');
+      $sql = "SELECT * FROM pharmacy";
+      $records = mysql_query($sql);
     ?>
+
+    <html>
+
+<body>
+	<table width="600" border="1">
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Adress</th>
+		</tr>
+
+		<tr>
+			<?php
+
+			while($pharmacy=mysql_fetch_assoc($records))
+			{
+				echo "<tr>";
+		
+				echo "<td>".$pharmacy['id']."</td>";
+				echo "<td>".$pharmacy['Name']."</td>";
+				echo "<td>".$pharmacy['Address']."</td>";
+				echo "</tr>";
+
+				$latitude2 = $pharmacy['Latitude'];
+				$longitude2 = $pharmacy['Longitude'];
+
+				$distance = getDistance($latitude, $longitude, $latitude2, $longitude2);
+
+				if($dist == 'None')
+					$dist = $distance;
+
+				if ($distance < $dist)
+				{
+					$dist = $distance;
+					$marker = $pharmacy['id'];
+				}
+
+
+			}
+
+			print "Shortest Distance: ";
+			print_r($dist);
+			print "<br><br>";
+
+			$sql2 = "SELECT Name,Address FROM pharmacy where id = $marker";
+			$result = mysql_query($sql2);
+
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) 
+			{
+    			print_r($row);
+			}
+
+			function getDistance($lat1, $long1, $lat2, $long2)
+			{
+				$earthRadius = 6371;
+				$latFrom = deg2rad($lat1);
+				$lonFrom = deg2rad($long1);
+				$latTo = deg2rad($lat2);
+				$lonTo = deg2rad($long2);
+
+				$latDelta = $latTo -$latFrom;
+				$lonDelta = $lonTo - $lonFrom;
+
+				$angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) * cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+
+				return $angle * $earthRadius;
+			}
+			?>
+		</tr>
+	</table>
+</body>
+
+</html>
